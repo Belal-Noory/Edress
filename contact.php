9f8eacef1258e44a5a3eff9498be70306503fcb6
Belal Noory <belalnoory2@gmail.com>
2022-09-28 14:18:48 -0700

    update

---
+++ contact.php
@@ -0,0 +1,297 @@
+
+<?php
+$title = "Contact";
+include("Master/header.php");
+?>
+<!-- slide background gradient -->
+
+<div class="page-header min-vh-100 mb-4" style="background-image: url('picture/header-background/contact-5.jpg');background-size: contain; background-repeat: no-repeat;  background-attachment:fixed">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <div class="text-center">
+            <h1 class=" animate__animated animate__bounceInRight"><span class="text-white border-radius-lg px-4 fs-1 bg-gradient-info" style="font-family:'Poppins' letter-spacing: 1px; font-weight:bold;">Contact Us </span></h1>
+          </div>
+
+          <div class="hero border-radius-lg shadow">
+            <h4 class="fs-4 bg-gradient-dark text-white px-4 border-radius-lg shadow" style="text-shadow: 2px 2px 2px black; font-weight: normal;">With Social Media</h4>
+
+            <div class="social-links mt-4 py-4">
+              <a href=""><i class="fab fa-facebook-f"></i></a>
+              <a href=""><i class="fab fa-instagram"></i></a>
+              <a href=""><i class="fab fa-whatsapp"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- title of section
+ -->
+
+
+
+
+
+      
+
+
+      
+
+      
+        
+
+
+
+
+
+
+
+
+     
+
+
+    
+<div class="accordion-1">
+  <div class="container">
+    <div class="row my-5">
+      <div class="col-md-12 mx-auto text-left">
+
+      <h3 class="text-danger mt-7 mb-4"> How Rudraksh Group Overseas Counseltancy Can Help You?</h3>
+      <section class="logo-section text-center space-small icon-section"><div class="container" bis_skin_checked="1"><div class="row" bis_skin_checked="1"><div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1"><div class="caption" bis_skin_checked="1"><img src="picture/icon/cic.png"  class="img-fluid w-50" alt="CICC-icon"><h5>CICC<br>Registered</h5></div></div><div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1"><div class="caption" bis_skin_checked="1"><img src="picture/icon/handhelper.png" class="img-fluid w-50" alt="Hand-holding-icon"><h5>Hand Holding Support</h5></div></div><div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1"><div class="caption" bis_skin_checked="1"><img src="picture/icon/transparent.png" class="img-fluid w-50" alt="Trust-transparent-icon"><h5>True &amp; Transparent</h5></div></div><div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1"><div class="caption" bis_skin_checked="1"><img src="picture/icon/time.png" class="img-fluid w-50" alt="Process-time-icon"><h5>Process Time 9-12 Months</h5></div></div></div></div></section>
+      <p style="color:black; font-size: 20px;">
+        We at Rudraksh Group have assisted thousands of individuals to successfully immigrate to Canada with their families through the Federal Express Entry Program. Our qualified and experienced Immigration Experts are equipped with the proficiency to accurately assess your case and advise the most suitable course of action for best serving your needs. 
+      </p>
+      <p style="color:black; font-size: 20px;">
+       Rudraksh Group has accumulated a treasure of goodwill and exhaustive experience over the last 14 years in this dynamic immigration industry and has constantly facilitated a wide range of aspiring immigrants to achieve their overseas goals. 
+      </p>
+      <p style="color:black; font-size: 20px;">
+       We are among the top-most brands in the Immigration sector being a highly reliable immigration firm with an outstanding track record of several thousands of triumphant PR Visa applications. The following major factors make us the Best Immigration Consultant for a Canada PR Visa: 
+      </p>
+
+      <figure>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          Our Consultation is offered by authorized and licensed Immigration Consultants
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          Step-by-Step guidance through eligibility and process
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          We maintain a Secure Online Process & transparency
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          System of Complaint Redress for any process
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          Experience of having processed more than 30,000 applications
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          Every Case is assessed multiple times before submission
+         </p>
+        </blockquote>
+        <blockquote class="blockquote">
+         <p class="ps-3" style="color:black; font-size: 18px;">
+          Easy evaluation of the qualifications and other skills
+         </p>
+        </blockquote>
+
+
+
+
+      </figure>
+
+
+
+
+    </div>
+
+
+
+
+    <!-- left section -->
+
+
+
+
+
+
+   
+
+
+
+<!-- About Section -->
+
+
+
+
+
+
+
+
+
+<!-- footer section -->
+
+
+
+
+
+
+
+
+
+<!-- -------- START Content Presentation Docs ------- -->
+<div style="background-image: url(picture/header-background/contact.jpg); background-size:cover; ">
+<div class="container mt-sm-5 mt-4">
+  <div class="page-header py-6 py-md-5 my-sm-3 mb-3 border-radius-xl blur shadow-blur" loading="lazy" >
+    <div class="container " >
+      <div class="row">
+        <div class="col-lg-6">
+          <img src="picture/header-background/contact-2.png" style="width: 600px; position: fixed; ">
+        </div >
+        <div class="col-lg-6 my-auto">
+          <div class="container py-4">
+    <div class="row">
+      <div class="col-lg-7 mx-auto d-flex justify-content-center flex-column">
+        <h3 class="text-center text-primary">Get in Touch</h3>
+        <form role="form" id="contact-form" method="post" autocomplete="off">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="input-group input-group-dynamic mb-4">
+                  <label class="form-label" style="color:black">First Name</label>
+                  <input class="form-control" aria-label="First Name..." type="text" >
+                </div>
+              </div>
+              <div class="col-md-6 ps-2">
+                <div class="input-group input-group-dynamic mb-4">
+                  <label class="form-label">Last Name</label>
+                  <input type="text" class="form-control" placeholder="" aria-label="Last Name..." >
+                </div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="input-group input-group-dynamic mb-4">
+                <label class="form-label">Email Address</label>
+                <input type="email" class="form-control">
+              </div>
+            </div>
+            <div class="input-group input-group-dynamic py-3">
+                <select class="form-select" aria-label="Visa Type">
+                    <option selected>Student Visa</option>
+                    <option value="1">Permenant Residency Visa</option>
+                    <option value="2">Work Visa</option>
+                    <option value="3">Tourist Visa</option>
+                    <option value="3">Business Visa</option>
+                    <option value="3">Dependent Visa</option>
+                    <option value="3">USA H-1B Visa</option>
+                    <option value="3">Canada Super Visa</option>
+                    <option value="3">USA O-1 Visa</option>
+                    <option value="3">UK Innovator Visa</option>
+                    <option value="3">USA EB-5 Visa</option>
+                  </select>
+            </div>
+            <div class="row">
+              <div class="col-md-12 mt-4">
+                <button type="submit" class="btn bg-gradient-dark w-100">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<!-- Contact Info -->
+
+
+<div class="container">
+  <div class="row mt-6">
+    <div class="col-lg-4">
+      <div class="info-horizontal bg-gradient-primary border-radius-xl d-block d-md-flex p-4 shadow">
+        <i class="fa fa-location text-white fs-2"></i>
+        <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+          <h5 class="text-white">Address</h5>
+          <p class="text-white">S.C.O.: 15 -16, Top Floor, Phase:1
+                                Sahibzada Ajit Singh Nagar, Punjab 160070
+                                India</p>
+          <a href="https://www.google.com/maps/dir//RD+group,+S.C.O.:+15+-16,+Top+Floor,+Phase:1,+Sahibzada+Ajit+Singh+Nagar,+Punjab+160070,+India/@30.7268292,76.6518091,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x390fee6e267ec513:0xe362c1a27b4bcfad!2m2!1d76.7218493!2d30.726848" target="_blank" class="text-white icon-move-right">
+            Let's start
+            <i class="fas fa-arrow-right text-sm ms-1"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+      <div class="col-lg-4">
+      <div class="info-horizontal bg-gradient-primary border-radius-xl d-block d-md-flex p-4 shadow">
+        <i class="fa-solid fa-square-phone text-white fs-2"></i>
+        <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+          <h5 class="text-white">Phone Number</h5>
+          <p class="text-white">S.C.O.: 15 -16, Top Floor, Phase:1
+                                Sahibzada Ajit Singh Nagar, Punjab 160070
+                                India</p>
+          <a href="https://www.google.com/maps/dir//RD+group,+S.C.O.:+15+-16,+Top+Floor,+Phase:1,+Sahibzada+Ajit+Singh+Nagar,+Punjab+160070,+India/@30.7268292,76.6518091,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x390fee6e267ec513:0xe362c1a27b4bcfad!2m2!1d76.7218493!2d30.726848" target="_blank" class="text-white icon-move-right">
+            Let's start
+            <i class="fas fa-arrow-right text-sm ms-1"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+      <div class="col-lg-4">
+      <div class="info-horizontal bg-gradient-primary border-radius-xl d-block d-md-flex p-4 shadow">
+        <i class="fa-solid fa-envelope text-white fs-2"></i>
+        <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+          <h5 class="text-white">Address</h5>
+          <p class="text-white">S.C.O.: 15 -16, Top Floor, Phase:1
+                                Sahibzada Ajit Singh Nagar, Punjab 160070
+                                India</p>
+          <a href="https://www.google.com/maps/dir//RD+group,+S.C.O.:+15+-16,+Top+Floor,+Phase:1,+Sahibzada+Ajit+Singh+Nagar,+Punjab+160070,+India/@30.7268292,76.6518091,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x390fee6e267ec513:0xe362c1a27b4bcfad!2m2!1d76.7218493!2d30.726848" target="_blank" class="text-white icon-move-right">
+            Let's start
+            <i class="fas fa-arrow-right text-sm ms-1"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- -------- END Content Presentation Docs ------- -->
+
+<?php
+include("Master/footer.php");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
