c5539d773bf17d081163fadf8475fb2061cea256
Edress <edres.med@gmail.com>
2022-09-29 08:20:19 -0700

    edress

---
+++ about.php
@@ -0,0 +1,464 @@
+<?php
+$title = "about";
+include("Master/header.php");
+?>
+
+
+
+
+  <!-- slide background gradient -->
+
+  <div class="page-header min-vh-80"
+    style="background-image: url('picture/header-background/bg-abo.jpg');background-attachment:fixed">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <div class="text-center">
+            <h3 class=" animate__animated animate__bounceInLeft">
+              <span class="text-white bg-gradient-dark border-radius-lg px-2 fs-1"
+                style="font-family:'Poppins'; letter-spacing: 1px; font-weight:normal; display: block;"><span
+                  class="bg-gradient-danger px-4 border-radius-lg shadow">Information About</span><br><b>Rudraksh</b>
+                Group Overseas Counseltancy</span>
+            </h3>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+  <div class="card card-body shadow-xl blur mx-3 mx-md-4 mt-n10 forhide text-center">
+
+    <section class="pt-3 pb-4 " id="count-stats">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9 mx-auto py-3">
+            <div class="row">
+              <div class="col-md-4 position-relative">
+                <div class="p-3 text-center">
+                  <h1 class="text-gradient text-primary"><span id="state1" countTo="100">0</span>%</h1>
+                  <h5 class="mt-3">Student Visa Success Rate</h5>
+                  <p class="text-sm font-weight-normal" style="color:black;">Canada - UK - Australia - USA - Italy<br>
+                    Germany - Singapore - New Zealand - Schengen - Poland - Malesia</p>
+                </div>
+                <hr class="vertical dark">
+              </div>
+              <div class="col-md-4 position-relative">
+                <div class="p-3 text-center">
+                  <h1 class="text-gradient text-primary"> <span id="state2" countTo="95">0</span>%</h1>
+                  <h5 class="mt-3">Work Visa</h5>
+                  <p class="text-sm font-weight-normal" style="color:black;">Canada - Malta - Portagal</p>
+                  <h5 class="mt-3">Permenant Residency Visa</h5>
+                  <p class="text-sm font-weight-normal" style="color:black;">Canada</p>
+                </div>
+                <hr class="vertical dark">
+              </div>
+              <div class="col-md-4">
+                <div class="p-3 text-center">
+                  <h1 class="text-gradient text-primary"><span id="state3" countTo="100">0</span>%</h1>
+                  <h5 class="mt-3">Business Visa</h5>
+                  <p class="text-sm font-weight-normal" style="color:black;">Canada - UK - Australia - USA - Italy -
+                    Dubai<br> Germany - Singapore - New Zealand - Schengen - Poland - Malesia</p>
+                </div>
+              </div>
+              <div class="section text-center">
+
+                <h2 class="title" id="about-page">Welcome to Rudraksh Group Overseas Counseltancy </h2>
+                <p style="color:black;">We are Professional Expert in Immigration Visa</p>
+                <h2></h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+
+  </div>
+
+
+
+
+
+  <!-- title of section
+ -->
+
+
+  <div class="container">
+    <div class="row mt-5 ">
+      <div class="card col-lg-12 px-4 py-4">
+        <div class="card-body">
+          <div>
+            <img src="picture/header-background/a-bg.png" width="100%">
+          </div>
+          <h4 class=" fs-3 bg-gradient-dark text-white px-4 border-radius-lg shadow" style="color: black;">Welcome To
+            Rudraksh Group Overseas Counseltancy</h4>
+
+          <p class="text-justify">
+            Rudraksh Group Overseas Solutions is one among the best immigration consultancy offering services for your
+            immigration needs. Who developed a business model, which provides solutions to all your immigration
+            concerns. With over 27 years of experience, we offer you a hassle-free transition to a new land. We provide
+            services to the people who want to settle, study, work or do business in countries like Australia, Canada,
+            New Zealand, UK and USA.
+          </p>
+
+
+
+          <div class="mt-8">
+            <img src="picture/header-background/mi.png" width="100%" class="border-radius-xl shadow">
+          </div>
+          <h2 class=" fs-3 " style="color: black;"><span
+              class="bg-gradient-dark text-white px-4 border-radius-lg shadow">Our Mission</span></h2>
+
+          <p class="text-justify mt-2">
+            Our team is committed to providing an efficient and cost-effective professional service. We support our
+            clients with timely and accurate immigration advice and assistance about immigration laws and legislation.
+          </p>
+          <p class="text-justify mt-4">
+            Our Team Rudraksh Group is a team of experienced & hard working members. Our people come from all over the
+            world, including Asia, the Mid-East, North America and Europe. Our staff members are trained in immigration
+            laws and procedures and have over 30 years of combined experience.
+          </p>
+
+
+
+
+
+          <h2 class=" fs-3 mt-8 " style="color: black;"> <span
+              class="bg-gradient-dark border-radius-lg shadow px-4 py-1 text-white">Why Us ?</span></h2>
+
+          <p class="text-justify mt-2">
+            It's quite simple we charge the institutions overseas for our services. Being an ethical operator, we do not
+            have the policy of double charging, i.e., charging the institutions overseas as well as charging you. We
+            only bill the institutions for our services thus saving your significant amount of money that you would
+            incur otherwise. Immigration to another country is a life changing decision. Getting good results – The
+            First Time is important!
+          </p>
+          <p class="text-justify mt-4">
+            At Rudraksh Group you'll find Immigration Consultants who are ethical, open and honest with every client.
+            Are qualified to interpret immigration laws and policy and advise you. Are experienced and competent to be
+            able to communicate with the relevant immigration department, embassy and visa officers on your behalf. Are
+            frank and honest about your prospects of success even before you retain
+          </p>
+
+
+          <div class="accordion-1">
+            <div class="container">
+              <div class="row my-5">
+                <div class="col-md-12 mx-auto text-left">
+                  <h3
+                    style="font-family: 'Tangerine', serif; font-size:40px; text-shadow:4px 4px 4px #aaa; color:black;">
+                    Why You Select Rudraksh Group Overseas Counseltancy</h3>
+
+                  <figure>
+                    <blockquote class="blockquote">
+                      <p class="ps-3">
+                        Are ethical, open and honest with every client.
+                      </p>
+                    </blockquote>
+                    <blockquote class="blockquote">
+                      <p class="ps-3">
+                        Are qualified to interpret immigration laws and policy and advise you.
+                      </p>
+                    </blockquote>
+                    <blockquote class="blockquote">
+                      <p class="ps-3">
+                        Are experienced and competent to be able to communicate with the relevant immigration
+                        department, embassy and visa officers on your behalf.
+                      </p>
+                    </blockquote>
+                    <blockquote class="blockquote">
+                      <p class="ps-3">
+                        Are frank and honest about your prospects of success even before you retain our services.
+                      </p>
+                    </blockquote>
+                  </figure>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container">
+      <h3 class="text-danger mt-7 mb-4" style="text-align:center"> How Rudraksh Group Overseas Counseltancy Can Help
+        You?</h3>
+      <section class="logo-section text-center space-small icon-section mt-6 shadow"
+        style=" border-right:4px solid red">
+        <div class="container" bis_skin_checked="1">
+          <div class="row" bis_skin_checked="1">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1">
+              <div class="caption" bis_skin_checked="1"><img src="picture/icon/cic.png" class="img-fluid w-50"
+                  alt="CICC-icon">
+                <h5>CICC<br>Registered</h5>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1">
+              <div class="caption" bis_skin_checked="1"><img src="picture/icon/handhelper.png" class="img-fluid w-50"
+                  alt="Hand-holding-icon">
+                <h5>Hand Holding Support</h5>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1">
+              <div class="caption" bis_skin_checked="1"><img src="picture/icon/transparent.png" class="img-fluid w-50"
+                  alt="Trust-transparent-icon">
+                <h5>True &amp; Transparent</h5>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-6" bis_skin_checked="1">
+              <div class="caption" bis_skin_checked="1"><img src="picture/icon/time.png" class="img-fluid w-50"
+                  alt="Process-time-icon">
+                <h5>Process Time 9-12 Months</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <p class="mt-4" style="color:black; font-size: 18px;">
+        We at Rudraksh Group have assisted thousands of individuals to successfully immigrate to Canada with their
+        families through the Federal Express Entry Program. Our qualified and experienced Immigration Experts are
+        equipped with the proficiency to accurately assess your case and advise the most suitable course of action for
+        best serving your needs.
+      </p>
+      <p style="color:black; font-size: 18px;">
+        Rudraksh Group has accumulated a treasure of goodwill and exhaustive experience over the last 14 years in this
+        dynamic immigration industry and has constantly facilitated a wide range of aspiring immigrants to achieve their
+        overseas goals.
+      </p>
+      <p style="color:black; font-size: 18px;">
+        We are among the top-most brands in the Immigration sector being a highly reliable immigration firm with an
+        outstanding track record of several thousands of triumphant PR Visa applications. The following major factors
+        make us the Best Immigration Consultant for a Canada PR Visa:
+      </p>
+
+      <figure>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            Our Consultation is offered by authorized and licensed Immigration Consultants
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            Step-by-Step guidance through eligibility and process
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            We maintain a Secure Online Process & transparency
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            System of Complaint Redress for any process
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            Experience of having processed more than 30,000 applications
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            Every Case is assessed multiple times before submission
+          </p>
+        </blockquote>
+        <blockquote class="blockquote">
+          <p class="ps-3" style="color:black; font-size: 18px;">
+            Easy evaluation of the qualifications and other skills
+          </p>
+        </blockquote>
+
+
+
+
+      </figure>
+
+
+
+
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+  <!-- -------- START Content Presentation Docs ------- -->
+
+
+  <!-- -------- START Content Presentation Docs ------- -->
+  <section style="background-image: url(picture/about/cc.jpg); background-size: cover;">
+    <div class="container py-4">
+      <div class="row">
+        <div
+          class="col-lg-4 mx-auto d-flex justify-content-center flex-column py-4 px-4 border-radius-lg opacity-9 shadow-blur"
+          style=" background-color:white;">
+          <h3 class="text-center">Contact us</h3>
+          <form role="form" id="contact-form" method="post" autocomplete="off">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="input-group input-group-dynamic mb-4">
+                    <label class="form-label">First Name</label>
+                    <input class="form-control" aria-label="First Name..." type="text">
+                  </div>
+                </div>
+                <div class="col-md-6 ps-2">
+                  <div class="input-group input-group-dynamic">
+                    <label class="form-label">Phone</label>
+                    <input type="text" class="form-control" placeholder="" aria-label="Last Name...">
+                  </div>
+                </div>
+              </div>
+              <div class="mb-4">
+                <div class="input-group input-group-dynamic">
+                  <label class="form-label">Email Address</label>
+                  <input type="email" class="form-control">
+                </div>
+              </div>
+              <div class="input-group input-group-dynamic ">
+                <select class="form-select text-center" aria-label="Visa Type">
+                  <option selected disabled>Visa Type</option>
+                  <option value="1">Student Visa</option>
+                  <option value="2">Permenant Residency Visa</option>
+                  <option value="3">Work Visa</option>
+                  <option value="4">Tourist Visa</option>
+                  <option value="5">Business Visa</option>
+                  <option value="6">Dependent Visa</option>
+                  <option value="7">USA H-1B Visa</option>
+                  <option value="8">Canada Super Visa</option>
+                  <option value="9">USA O-1 Visa</option>
+                  <option value="10">UK Innovator Visa</option>
+                  <option value="11">USA EB-5 Visa</option>
+                </select>
+              </div>
+
+              <div class="input-group input-group-dynamic mt-4 ">
+                <select class="form-select text-center" aria-label="Country">
+                  <option selected disabled>Select Country</option>
+                  <option value="1">Canada</option>
+                  <option value="2">UK</option>
+                  <option value="3">Australia</option>
+                  <option value="4">Germany</option>
+                  <option value="5">USA</option>
+                  <option value="6">Singapore</option>
+                  <option value="7">Poland</option>
+                  <option value="8">Europe</option>
+                  <option value="9">Italy</option>
+                  <option value="10">Malta</option>
+                  <option value="11">New Zealand</option>
+                  <option value="11">Schengen</option>
+                  <option value="11">Malesia</option>
+                </select>
+              </div>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-check form-switch mb-4 d-flex align-items-center">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked="">
+                    <label class="form-check-label ms-3 mb-0" for="flexSwitchCheckDefault">I agree to the <a
+                        class="text-dark"><u>Terms and Conditions</u></a>.</label>
+                  </div>
+                </div>
+                <div class="col-md-6" style="margin: 0px auto;">
+                  <button type="submit" class="btn bg-gradient-dark w-100">Submit</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Contact Info -->
+
+
+  <div class="container">
+    <div class="row mt-6">
+      <div class="col-lg-4 mt-6">
+        <div class="info-horizontal bg-gradient-primary border-radius-xl d-block d-md-flex p-4 shadow">
+          <i class="fa fa-location text-white fs-2"></i>
+          <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+            <h5 class="text-white">Address</h5>
+            <p class="text-white" style="font-size:14px">S.C.O.: 15 -16, Top Floor, Phase:1
+              Sahibzada Ajit Singh Nagar, Punjab 160070
+              India</p>
+            <a href="https://www.google.com/maps/place/RD+group/@30.728164,76.722346,15z/data=!4m5!3m4!1s0x0:0xe362c1a27b4bcfad!8m2!3d30.728094!4d76.7223167?hl=en"
+              target="_blank" class="text-white icon-move-right font-weight-bold">
+              Show Location
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mt-6">
+        <div class="info-horizontal shadow  border-radius-xl d-block d-md-flex p-4 shadow">
+          <i class="fa-solid fa-square-phone text-primary fs-2"></i>
+          <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+            <h5 class="text-primary">Phone Number</h5>
+            <p><a href="tel:+91 1725063767" class="text-dark fs-4">1725063767</a></p>
+            <a href="tel:+91 1725063767" class="text-primary icon-move-right font-weight-bold">
+              Call Now
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 mt-6">
+        <div class="info-horizontal shadow border-radius-xl d-block d-md-flex p-4 shadow">
+          <i class="fa-solid fa-envelope text-primary fs-2"></i>
+          <div class="ps-0 ps-md-3 mt-3 mt-md-0">
+            <h5 class="text-primary fs-4">Email Address</h5>
+            <p class="text-dark">Info@rudrakshgroup.co.in</p>
+            <a href="mailto:Info@rudrakshgroup.co.in" target="_blank"
+              class="text-primary icon-move-right font-weight-bold">
+              Click Here
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- -------- END Content Presentation Docs ------- -->
+
+  <?php
+  include("Master/footer.php");
+  ?>
